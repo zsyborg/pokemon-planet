7b662d6ab1e668c6a3d4cdb5021ed4f264d3314d
Zasha <zahid.shaikh@live.com>
2026-02-15 22:26:03 +0530

    Updated Database URL

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Db, Collection } from "mongodb";
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const uri = process.env.MONGODB_URI || "mongodb+srv://techzasha:Ajna4028@dharti.ctgvhra.mongodb.net/Pokemon?appName=Dharti";
 
 let client: MongoClient;
 let db: Db;
